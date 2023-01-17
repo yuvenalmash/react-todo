168aa0898de141ffe0a8da2071e38cf5be33ef92
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-17 23:36:12 +0300

    refactor to function based

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // component file
-import TodoContainer from "./classBased/components/TodoContainer";
+import TodoContainer from "./functionBased/components/TodoContainer";
 
 //stylesheet
-import "./classBased/App.css";
+import "./functionBased/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
