1aea40f85939028382d5c1f5d89fdde5d44d2fb7
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-17 14:40:03 +0300

    Destructure todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,19 +9,21 @@ function TodoItem(props) {
     textDecoration: "line-through",
   };
 
+  const { completed, id, title } = props.todo;
+
   return (
     <li className={styles.item}>
       <input
         type="checkbox"
         className={styles.checkbox}
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
       />
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+      <button onClick={() => props.deleteTodoProps(id)}>
         Delete
       </button>
-      <span style={props.todo.completed ? completedStyle : null}>
-        {props.todo.title}
+      <span style={completed ? completedStyle : null}>
+        {title}
       </span>
     </li>
   );
